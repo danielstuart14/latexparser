08ffeb7bec64ce231554b0d7997065502252827b
Daniel Stuart <daniel.stuart@pucpr.edu.br>
2020-11-17 23:14:41 -0300

    Implement emcc size optimization

--- wasmCompile.sh
+++ wasmCompile.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-emcc ./cparse/functions.cpp -c -o ./cparse/functions.o
-emcc ./cparse/packToken.cpp -c -o ./cparse/packToken.o
-emcc ./cparse/shunting-yard.cpp -c -o ./cparse/shunting-yard.o
-emcc ./cparse/containers.cpp -c -o ./cparse/containers.o
-emcc ./cparse/builtin-features.cpp -c -o ./cparse/builtin-features.o
+emcc -Os ./cparse/functions.cpp -c -o ./cparse/functions.o
+emcc -Os ./cparse/packToken.cpp -c -o ./cparse/packToken.o
+emcc -Os ./cparse/shunting-yard.cpp -c -o ./cparse/shunting-yard.o
+emcc -Os ./cparse/containers.cpp -c -o ./cparse/containers.o
 
-emcc ./latexParser/sanitize.cpp -c -o ./latexParser/sanitize.o
-emcc ./latexParser/solver.cpp -c -o ./latexParser/solver.o
+emcc -Os ./latexParser/sanitize.cpp -c -o ./latexParser/sanitize.o
+emcc -Os ./latexParser/solver.cpp -c -o ./latexParser/solver.o
 
 rm -rf ./out
 mkdir ./out
 
-emcc -I ./cparse ./latexParser/main.cpp ./cparse/{functions.o,packToken.o,shunting-yard.o,containers.o,builtin-features.o} ./latexParser/{sanitize.o,solver.o} --bind -o ./out/latexParser.js
+emcc -Os --closure 1 -I ./cparse ./latexParser/main.cpp ./cparse/{functions.o,packToken.o,shunting-yard.o,containers.o} ./latexParser/{sanitize.o,solver.o} --bind -o ./out/latexParser.js
